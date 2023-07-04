ba20fcdf7d711c106db20ee3fca007ba6ef5a597
saeedeht <saeedehtorkamani@gmail.com>
2023-07-03 22:01:01 -0700

    added footer and header to app so it wont repit in every singel page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Loading from "./components/Loading";
 import MyBins from "./components/MyBins";
 import Recycling from "./components/Recycling";
@@ -10,6 +11,7 @@ import Scan from "./components/Scan";
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Loading" element={<Loading />} />
@@ -17,6 +19,7 @@ function App() {
         <Route path="/Recycling" element={<Recycling />} />
         <Route path="/Scan" element={<Scan />} />
       </Routes>
+      <Footer />
     </Router>
     //fix header there
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,59 +1,57 @@
 import React from "react";
 import { Grid, Typography } from "@mui/material";
-import Footer from "./Footer";
-import Header from "./Header";
-
+// import Footer from "./Footer";
+// import Header from "./Header";
 
 function Home() {
-    return (
+  return (
+    <Grid
+      xs={12}
+      container
+      sx={(t) => ({
+        backgroundColor: t.palette.primary.main,
+        display: "flex",
+        // position: "absolute",
+      })}>
+      {/* <Header /> */}
+      <Grid
+        xs={12}
+        container
+        sx={(t) => ({
+          backgroundColor: t.palette.primary.main,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          py: 6,
+          px: 4,
+          // position:"absolute",
+        })}>
         <Grid
-            xs={12}
-            container 
-            sx={(t) => ({
-            backgroundColor: t.palette.primary.main,
+          item
+          xs={12}
+          sx={(t) => ({
+            backgroundColor: t.palette.primary.light,
+            borderRadius: "13px",
             display: "flex",
-            position:"absolute",
-        })}
-        >
-            <Header />
-            <Grid
-                xs={12}
-                container 
-                sx={(t) => ({
-                backgroundColor: t.palette.primary.main,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                py: 6,
-                px: 4,
-                // position:"absolute",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            py: 23,
+            px: 13,
+          })}>
+          <Typography
+            sx={(t) => ({
+              color: t.palette.primary.main,
+              fontSize: "18px",
+              fontWeight: "700",
             })}>
-                <Grid
-                    item
-                    xs={12}
-                    sx={(t) => ({
-                    backgroundColor: t.palette.primary.light,
-                    borderRadius: "13px",
-                    display: "flex",
-                    flexDirection:"column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    py: 23,
-                    px: 13,
-                    
-                    })}>
-                        <Typography sx={(t) => ({
-                            color: t.palette.primary.main,
-                            fontSize: "18px",
-                            fontWeight: "700",
-                        })}>
-                            HOME
-                    </Typography>
-                </Grid>
-            </Grid>
-            <Footer />  
+            HOME
+          </Typography>
         </Grid>
-    )
+      </Grid>
+      {/* <Footer />   */}
+    </Grid>
+  );
 }
 
 export default Home;
